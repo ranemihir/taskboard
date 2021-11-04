4111e1c7cdb7fbf5cc17681e2b1194b111ee69f3
ranemihir <mihirrane171@gmail.com>
2021-11-04 12:41:16 +0530

    minor design changes

--- src/app/taskboard/kanban-board/kanban-board.component.ts
+++ src/app/taskboard/kanban-board/kanban-board.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AppState } from 'src/app/shared/state';
-import { Project, ProjectRole, Task } from 'src/app/shared/types';
+import { CurrentUser, Project, ProjectRole, Task } from 'src/app/shared/types';
 import { environment } from 'src/environments/environment';
 import * as ProjectsActions from './../state/projects/projects.actions';
 import * as ProjectsSelectors from './../state/projects/projects.selectors';
@@ -10,6 +10,7 @@ import * as ProjectRolesSelectors from './../state/project_roles/project_roles.s
 import * as TasksSelectors from './../state/tasks/tasks.selectors';
 import * as TasksActions from 'src/app/taskboard/state/tasks/tasks.actions';
 import * as ProjectRolesActions from 'src/app/taskboard/state/project_roles/project_roles.actions';
+import * as CurrentUserSelectors from 'src/app/auth/state/current_user/current_user.selectors';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -17,10 +18,12 @@ import { map } from 'rxjs/operators';
   templateUrl: './kanban-board.component.html',
   styleUrls: ['./kanban-board.component.scss']
 })
-export class KanbanBoardComponent implements OnInit {
+export class KanbanBoardComponent implements OnInit, OnDestroy {
   project$: Observable<Project>;
   projectRolesMap: { [key: string]: ProjectRole; };
+  projectRolesMapSub: Subscription;
   tasksMap$: Observable<{ [key: string]: Task[]; }>;
+  currentUser$: Observable<CurrentUser>;
 
   avatarUrl: string = environment.avatarUrl;
 
@@ -35,7 +38,7 @@ export class KanbanBoardComponent implements OnInit {
       })
     );
 
-    this.store.select(ProjectRolesSelectors.fetchAllProjectRolesOfProject).pipe(
+    this.projectRolesMapSub = this.store.select(ProjectRolesSelectors.fetchAllProjectRolesOfProject).pipe(
       map((projectRoles: ProjectRole[]) => {
         const projectRolesMap: { [key: string]: ProjectRole; } = {};
 
@@ -66,9 +69,15 @@ export class KanbanBoardComponent implements OnInit {
       })
     );
 
+    this.currentUser$ = this.store.select(CurrentUserSelectors.get);
+
   }
 
   ngOnInit(): void {
+    this.projectRolesMapSub.unsubscribe();
+  }
+
+  ngOnDestroy(): void {
 
   }
 
